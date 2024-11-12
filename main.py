5434243d050bee22e9ac0781c5a319dab2f4a883
James William Chamberlain <jw.chamberlain@outlook.com>
2024-11-12 02:13:46 +0000

    Removed Unnecessary Code
    Removed what I believe to be unnecessary code

--- main.py
+++ main.py
@@ -19,79 +19,75 @@ from sklearn.ensemble import RandomForestRegressor
 # ==============================================================================================================
 # ==============================================================================================================
 # ==============================================================================================================
-# ref. https://github.com/anyoptimization/pymoo/blob/main/pymoo/algorithms/moo/sms.py#L14 
-# ==============================================================================================================
-# ==============================================================================================================
-# ==============================================================================================================
+
 
 # custom survival function - this is the main class that is extended from the pymoo library
+# ref. https://github.com/anyoptimization/pymoo/blob/main/pymoo/algorithms/moo/sms.py#L14 
 
 from pymoo.algorithms.moo.sms import LeastHypervolumeContributionSurvival
 
 class MyLeastHypervolumeContributionSurvival(LeastHypervolumeContributionSurvival):
-
     def __init__(self, eps=10.0) -> None:
         super().__init__(eps=eps)
 
-    # def _do # todo
+    def _do(self, problem, pop, *args, n_survive=None, ideal=None, nadir=None, **kwargs):
+        return super()._do(problem, pop, *args, n_survive=n_survive, ideal=ideal, nadir=nadir, **kwargs)
 
+        
 # ==============================================================================================================
 # ==============================================================================================================
 # ==============================================================================================================
 
 class HySMSEMOA(SMSEMOA):
 
-    __model__ = None
-    __model_initialized__ = False
-
-    # def next(self):
-    #     return super().next()
+    # __model__ = None
+    # __model_initialized__ = False
 
     def __init__(self):
-        super().__init__()
-        # super().__init__(survival=MyLeastHypervolumeContributionSurvival())
+        # super().__init__()
+        super().__init__(survival=MyLeastHypervolumeContributionSurvival())
   
-    def suggest_solutions(self, num_suggestions, n_var, xu, xl):
-        xu = np.array(xu).reshape(1, n_var)
-        xl = np.array(xl).reshape(1, n_var)
+    # def suggest_solutions(self, num_suggestions, n_var, xu, xl):
+    #     xu = np.array(xu).reshape(1, n_var)
+    #     xl = np.array(xl).reshape(1, n_var)
 
-        # Generate random values within bounds
-        X_new = np.random.rand(num_suggestions, n_var) * (xu - xl) + xl
-        return X_new
+    #     # Generate random values within bounds
+    #     X_new = np.random.rand(num_suggestions, n_var) * (xu - xl) + xl
+    #     return X_new
 
 
     def _advance(self, infills=None, **kwargs):
         # messing with infills to understand the data structure
-        if infills is not None:
-            # further training of the model
-            if not self.__model_initialized__:
-                # initialize the model
-                self.__model_initialized__ = True
-                self.__model__ = RandomForestRegressor(n_estimators=100)
-                self.__model__.fit(infills.get("X"), infills.get("F"))
-            else:
-                # update the model
-                self.__model__.fit(infills.get("X"), infills.get("F"))
+        # if infills is not None:
+        #     # further training of the model
+        #     if not self.__model_initialized__:
+        #         # initialize the model
+        #         self.__model_initialized__ = True
+        #         self.__model__ = RandomForestRegressor(n_estimators=100)
+        #         self.__model__.fit(infills.get("X"), infills.get("F"))
+        #     else:
+        #         # update the model
+        #         self.__model__.fit(infills.get("X"), infills.get("F"))
 
 
-            print(infills.get("X").shape) # (total num of offspring, D (i.e., num of decision variables))
+        #     print(infills.get("X").shape) # (total num of offspring, D (i.e., num of decision variables))
 
-            # predict the scores
-            _predicted_scores = self.__model__.predict(infills.get("X"))
+        #     # predict the scores
+        #     _predicted_scores = self.__model__.predict(infills.get("X"))
 
-            # suggest new solutions
-            X_new = self.suggest_solutions(1000, 30, problem.xu, problem.xl)
-            y_new = self.__model__.predict(X_new)
-            # y_new = self.__model__.predict(X_new)
+        #     # suggest new solutions
+        #     X_new = self.suggest_solutions(1000, 30, problem.xu, problem.xl)
+        #     y_new = self.__model__.predict(X_new)
+        #     # y_new = self.__model__.predict(X_new)
 
-            # plot actual vs predicted scores
-            plot = Scatter()
-            plot.add(infills.get("F"), color="blue")
-            plot.add(_predicted_scores, color="red")
-            plot.add(y_new, color="green")
-            # problem (may not be available so default is commented out)
-            plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
-            plot.show()
+        #     # plot actual vs predicted scores
+        #     plot = Scatter()
+        #     plot.add(infills.get("F"), color="blue")
+        #     plot.add(_predicted_scores, color="red")
+        #     plot.add(y_new, color="green")
+        #     # problem (may not be available so default is commented out)
+        #     plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
+        #     plot.show()
 
         return super()._advance(infills, **kwargs) # type: ignore
     
@@ -148,5 +144,3 @@ print(X)
 #     plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
 #     plot.add(res.F, color="red")
 #     plot.show()
-
-# TODO look into survival function - as this could be useful for improving this algorithm (or atleast attempting to for this assignment)
