bee22fcc8632901f794aa1ead87654d5c104c246
James William Chamberlain <jw.chamberlain@outlook.com>
2024-11-13 18:55:44 +0000

    Added Convergence Dict
    Dictionary for when they have visibly converged to nearest 10 generations

--- analysis_default.py
+++ analysis_default.py
@@ -9,22 +9,45 @@ from pymoo.indicators.hv import HV
 D = [5, 10, 30]                                                 # number of decision variables (hav to test for each value)
 problems = ["zdt1", "zdt3", "zdt4", "zdt6"]                     # benchmark problems to test
 
+convergance_dict = {
+    "zdt1": [40, 70, 140],
+    "zdt3": [40, 70, 120],
+    "zdt4": [130, 190, 670],
+    "zdt6": [130, 230, 600]
+}
+
 # SMS-EMOA
 for problem in problems:
     for d in D:
         p = get_problem(problem, n_var=d)
+        n_gen = convergance_dict[problem][D.index(d)]
         algorithm = SMSEMOA() 
         res = minimize(p,
                        algorithm,
-                       ('n_gen', 100),
+                       ('n_gen', n_gen),
                        seed=1,
                        verbose=False,
                        save_history=False)
         
         ind = HV(ref_point=[1.1, 1.1])
-        if problem == "zdt4":
-            plot = Scatter()
-            plot.add(p.pareto_front(), plot_type="line", color="black", alpha=0.7)
-            plot.add(res.F, color="red")
-            plot.show()
-        print(f"{problem}, {d}: {ind(res.F)}")
+        # if problem == "zdt4" or problem == "zdt6":
+        #     plot = Scatter()
+        #     plot.add(p.pareto_front(), plot_type="line", color="black", alpha=0.7)
+        #     plot.add(res.F, color="red")
+        #     plot.show()
+        print(f"{problem}, {d}: {ind(res.F)},  at {n_gen} generations")
+
+# Checking Convergance Manually
+# p = get_problem("zdt6", n_var=30)
+# algorithm = SMSEMOA() 
+# res = minimize(p,
+#                algorithm,
+#                ('n_gen', 600),
+#                seed=1,
+#                verbose=False,
+#                save_history=False)
+
+# plot = Scatter()
+# plot.add(p.pareto_front(), plot_type="line", color="black", alpha=0.7)
+# plot.add(res.F, color="red")
+# plot.show()
